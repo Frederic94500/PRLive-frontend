b842a78c2408cb3848f80b2cd8cfa4d0114e101d
Frédéric TRAN <frederictran@hotmail.fr>
2024-08-19 23:17:28 +0200

    feat: Add Guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { AuthService } from "../services/auth.service";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Injectable()
+export class AuthGuard {
+  constructor(private router: Router) {}
+  
+  public async canActivate(): Promise<boolean> {
+    const authService = new AuthService();
+    if ((await authService.getWhoAmI()).code !== 200) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+}
