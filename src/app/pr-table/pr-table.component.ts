67cd97383625f89d33ed089778df80312727d656
Frédéric TRAN <frederictran@hotmail.fr>
2024-09-14 23:16:58 +0200

    feat: Download and Delete button

--- src/app/pr-table/pr-table.component.ts
+++ src/app/pr-table/pr-table.component.ts
@@ -6,7 +6,10 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { PRModel } from '@models/pr.model';
+import { PROutput } from '@/src/interfaces/pr.interface';
+import { PRService } from '@/src/services/pr.service';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -34,6 +37,8 @@ export class PRTableComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatSort) sort!: MatSort;
 
+  constructor(private snackBar: MatSnackBar) {}
+
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
@@ -48,5 +53,32 @@ export class PRTableComponent implements OnInit, AfterViewInit {
       return 'red-value';
     }
     return '';
+  }
+
+  async downloadJson(prId: string): Promise<void> {
+    const prOutput: PROutput =  (await (new PRService().outputPR(prId))).data;
+    const json = JSON.stringify(prOutput, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${prOutput.name}_output_PR.json`;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  openDeleteDialog(prId: string): void {
+    confirm('Are you sure you want to delete this PR?') ? this.deletePR(prId) : null;
+  }
+
+  async deletePR(prId: string): Promise<void> {
+    const response = await new PRService().deletePR(prId);
+    if (response.code !== 200) {
+      this.snackBar.open('Error deleting PR', 'Close', { duration: 2000 });
+      return;
+    }
+    this.snackBar.open('PR deleted', 'Close', { duration: 2000 });
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    window.location.reload();
   }
 }
